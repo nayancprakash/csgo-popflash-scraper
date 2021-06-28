2062fa6f04913cc5b36b123e9d52b45e2ed6cbd4
Nayan Prakash <nayanprakash2@gmail.com>
2021-06-28 07:23:51 -0600

    feat: add general match statistics

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     scoreboard = soup.find(class_="scoreboards")
     stats = {}
-    for table in scoreboard.find_all("table"):
+    for table_count, table in enumerate(scoreboard.find_all("table")):
         stat_names = []
         for y, row in enumerate(table.find_all("tr")):
             player = None
@@ -33,6 +33,7 @@ if __name__ == "__main__":
                 elif y > 0 and x > 0:
                     if player not in stats:
                         stats[player] = {}
+                        stats[player]["Team"] = (table_count // 4) + 1
                     stats[player][stat_names[x-1]] = data.text
 
     csv_lines = [["Names"]]
@@ -48,3 +49,16 @@ if __name__ == "__main__":
     writer = csv.writer(open(match_id + ".csv", "w", newline=""))
     for row in csv_lines:
         writer.writerow(row)
+
+    match_text = soup.find(id="match-container").find_all("div")[0].find_all("div")[0].text.split()
+    scores = match_text[0:2]
+    map_name = match_text[3]
+    date = soup.find(id="match-container").find_all("span")[0].get("data-date")
+    
+    match_stats = {}
+    match_stats["match_id"] = match_id
+    match_stats["match_date"] = date
+    match_stats["map"] = map_name
+    match_stats["scores"] = scores
+    match_stats["player_stats"] = stats
+    print(match_stats)
